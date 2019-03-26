a1657d600758058afbaf87b0dc5cf0edfa300311
Alina <alex.alina.rusu@gmail.com>
2019-03-26 14:04:38 +0100

    initial commit, project setup, file structure setup

---
+++ src/components/todolist/TodoListContainer.js
@@ -0,0 +1,12 @@
+import React, {PureComponent} from 'react';
+
+class TodoListContainer extends PureComponent {
+
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default TodoListContainer;
